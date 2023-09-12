4c66423bd62d0148087e49d7b231766151d84bfa
Vasiliy <vasiliy.trunov.00@mail.ru>
2023-09-12 21:28:34 +0200

    Прописан игровой класс маг; Доделан игровой класс стрелок

---
+++ mage.py
@@ -0,0 +1,117 @@
+from sample import Sample
+from time import time
+from random import randrange
+
+class Mage(Sample):
+
+    def __init__(self, name: str):
+        super().__init__(name)
+        
+        self.mana=0
+        self.max_mana=100
+        
+        self.recovery_mana=15
+        self.recovery_time=1.5
+        self.recovery_mana_current=time()
+
+        self.recovery_fireBall=3
+        self.recovery_fireBall_current=time()
+
+        self.recovery_magicShield=4
+        self.recovery_magicShield_current=time()
+        self.duratoin_magicShield=3
+        self.duratoin_magicShield_current=0
+        self.magicShield_flag = False
+
+        self.magicExplocion_count=0
+        self.max_count=5
+        self.stored_damage=0
+
+        self.armor-=3
+        self.damage-=2
+
+        
+    def giveDamage(self):
+        self.recoveryMana()
+        result_damage = self.magicExplocion()
+        if result_damage!=0:
+            return result_damage
+
+        self.magicShield()
+
+        result_damage=self.fireBall()
+        if result_damage!=0:
+            return result_damage
+        else:
+            result_damage = super().giveDamage
+       
+        return result_damage
+
+
+    def getDamage(self, value: int):
+        self.recoveryMana()
+        if self.magicShield_flag:
+            back_damage = value*0.15
+            self.stored_damage+=back_damage
+            self.target.getDamage(back_damage)
+            self.magicCountMax(back_damage)
+        super().getDamage(value)
+        
+
+    def recoveryMana(self):
+        #восстановление маны
+        self.mana+=(time()-self.recovery_mana_current)/self.recovery_time*self.recovery_mana
+        
+
+    def magicShield(self):
+        #магический шит
+        if time()-self.recovery_magicShield_current>=self.recovery_magicShield and self.mana>=30:
+            self.recovery_magicShield_current=time()
+            self.mana-=30
+            self.magicShieldOn()
+            self.magicCountMax()
+    
+
+    def magicShieldOn(self):
+        #включение магического щита
+        if self.duratoin_magicShield==0:
+            self.duratoin_magicShield=time()
+            self.armor+=8
+            self.magicShield_flag=True
+
+            
+    def magicShieldOf(self):
+        #выключение магического щита
+        if time()-self.duratoin_magicShield_current>=self.duratoin_magicShield:
+            self.recovery_magicShield_current=0
+            self.armor-=8
+            self.magicShield_flag=False
+
+
+    def fireBall(self):
+        #огненный шар
+        if time()-self.recovery_fireBall_current>=self.recovery_fireBall and self.mana>=25:
+            self.mana-=25
+            self.recovery_fireBall_current=time()
+            result_damage = super().giveDamage()*(randrange(15, 21)/10)
+            self.magicCountMax(result_damage)
+            return result_damage
+        else: return 0
+
+
+    def magicCountMax(self, damage:int):
+        # прибавка к зарядам магического взрыва
+        if self.magicExplocion_count<self.max_count:
+            self.magicExplocion_count+=1
+            self.stored_damage+=damage
+            
+
+    def magicExplocion(self):
+        #Магический взрыв
+        if self.magicExplocion_count==self.max_count and self.mana>=60:
+            self.mana-=60
+            self.magicExplocion_count=0
+            result_damage = self.stored_damage
+            self.stored_damage=0
+            return result_damage
+        else: return 0
--- shooter.py
+++ shooter.py
@@ -13,30 +13,76 @@ class Shooter(Sample):
 
             self.recharge_cripplingShot = 4
             self.recharge_cripplingShot_current = time()
+            self.duration_cripplingShotBaff = 2
+            self.duration_cripplingShotBaff_current = 0
+            self.crippled_speed = 0
+            self.crippled_damage = 0
+            
 
             self.recharge_sharpEye = 5
             self.recharge_sharpEye_current = time()
             
 
             self.armor-=3
-            self.damage-=4
+            self.damage-=5
 
             self.speed+=25
+            self.increase_speed_current=0
+            self.increase_speed = 5
 
     def giveDamage(self):
-        result_damage = self.sharpEye()
-        if self.sharpEye()!=0: return result_damage
+        self.passion+=15
+        self.cripplingShotBaffOf()
+        sharpEye_damage = self.sharpEye()
+        if self.sharpEye()!=0:
+            return sharpEye_damage
         else:
-            pass
-            #ДОПИСАТЬ 
+            return super().giveDamage()+self.cripplingShot()
+
+    def getDamage(self):
+        self.passion+=5
+        super().getDamage()
     
 
     def cripplingShot(self):
-        
-        pass
+        # калечащий выстрел
+        if time()-self.recharge_cripplingShot_current>=self.recharge_cripplingShot and self.passion>=30:
+            self.recharge_cripplingShot_current = time()
+            self.passion-=30
+            result_damage = 5+ self.target.hp*0.05
+            self.cripplingShotBaffOn()
+            return result_damage
+
+    def cripplingShotBaffOn(self):
+        #включение усиления от колечащего выстрела
+        if self.duration_cripplingShotBaff_current==0:
+            self.duration_cripplingShotBaff_current=time()
+            self.burst_damage+=0.2
+            self.crippled_speed=self.target.speed*0.2
+            self.crippled_damage=self.target.damage*0.2
+            
+            self.target.speed-=self.crippled_speed
+            self.target.damage-=self.crippled_damage
+            
+
+    def cripplingShotBaffOf(self):
+        #выключение усиления от колечащего выстрела
+        if time()-self.duration_cripplingShotBaff_current>=self.duration_cripplingShotBaff and self.duration_cripplingShotBaff_current!=0:
+            self.duration_cripplingShotBaff_current=0
+            self.burst_damage-=0.2
+            self.target.speed+=self.crippled_speed
+            self.target.damage+=self.crippled_damage
+            
+
+    def passionForShooting(self):
+        # пассивная прибавка к скорости атаки от количества азарта(passion) 
+        self.speed-=self.increase_speed_current
+        self.increase_speed_current=(self.passion//10*self.increase_speed)
+        self.speed+=self.increase_speed_current
 
 
     def increasedPerformanceDegradation(self, value=1):
+        # при value=1 прибавляет характеристики, при -1 отнимает
         self.lack+=randrange(20, 50)*value
         self.crete+=randrange(1,4)*value
 
